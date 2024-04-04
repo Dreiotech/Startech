c086a8e1661b10dbea9bc2a26fe7c32b32511a75
Dreiotech <yonidreio3@gmail.com>
2024-04-04 10:20:43 +0300

    initial commit

--- app/src/main/java/com/kimm369/startech/RegisterActivity.kt
+++ app/src/main/java/com/kimm369/startech/RegisterActivity.kt
@@ -1,9 +1,6 @@
 package com.kimm369.startech
 
-import android.content.Intent
-import android.graphics.drawable.Icon
 import android.os.Bundle
-import android.support.v4.os.IResultReceiver.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -17,7 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -33,7 +32,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +39,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kimm369.startech.ui.theme.StartechTheme
 
-class MainActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,19 +49,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   // Greeting("Android")
-                    login()
+                   register()
                 }
             }
         }
     }
 }
-
 @Composable
-fun login() {
+fun register() {
+    var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var context = LocalContext.current
+    var confirmpassword by remember { mutableStateOf("") }
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
@@ -77,24 +74,52 @@ fun login() {
             fontFamily = FontFamily.Cursive
         )
         OutlinedTextField(
-            value = email ,
-            onValueChange = {email = it},
-            leadingIcon = { Icon(imageVector = Icons.Default.Email,
-                contentDescription = "EMAIL")},
-            label = { Text(text = "Email Address",color = Color.Cyan, fontSize = 20.sp)},
+            value = name ,
+            onValueChange = {name = it},
+            leadingIcon = { Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "name")},
+            label = { Text(text = "Enter your name",color = Color.Cyan, fontSize = 20.sp)},
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp),
 
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
             keyboardActions = KeyboardActions()
-            )
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            leadingIcon = { Icon(
+                imageVector = Icons.Default.Email ,
+                contentDescription = "email")},
+            label = { Text(text = "Email Address",color = Color.Cyan, fontSize = 20.sp)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions()
+        )
         OutlinedTextField(
             value = password,
             onValueChange = {password = it},
-            leadingIcon = { Icon(imageVector = Icons.Default.Person ,
+            leadingIcon = { Icon(
+                imageVector = Icons.Default.Lock,
                 contentDescription = "")},
             label = { Text(text = "Password",color = Color.Cyan, fontSize = 20.sp)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions()
+        )
+        OutlinedTextField(
+            value = confirmpassword,
+            onValueChange = {confirmpassword = it},
+            leadingIcon = { Icon(
+                imageVector = Icons.Default.Done,
+                contentDescription ="" )},
+            label = { Text(text = "Confirm Password",color = Color.Cyan, fontSize = 20.sp)},
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp),
@@ -104,54 +129,21 @@ fun login() {
         Spacer(modifier = Modifier.height(30.dp))
         Button(onClick = { /*TODO*/ },
             modifier = Modifier.fillMaxWidth()) {
-          
-            Text(text = "Login",
+
+            Text(text = "Register",
                 fontSize = 25.sp)
         }
         Spacer(modifier = Modifier.height(30.dp))
-        Button(onClick = {
-                         val intent =Intent(context,RegisterActivity::class.java)
-                         context.startActivity(intent)
-
-        },
+        Button(onClick = { /*TODO*/ },
             modifier = Modifier.fillMaxWidth()) {
 
-            Text(text = "Click to Register",
+            Text(text = "Click to Login",
                 fontSize = 25.sp)
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        Button(onClick = {
-            val intent =Intent(context,IntentActivity::class.java)
-            context.startActivity(intent)
-
-        },
-            modifier = Modifier.fillMaxWidth()) {
-
-            Text(text = "Click to Intent",
-                fontSize = 25.sp)
-        }
-        Spacer(modifier = Modifier.height(30.dp))
-        Button(onClick = {
-            val intent =Intent(context,Simplelist::class.java)
-            context.startActivity(intent)
-
-        },
-            modifier = Modifier.fillMaxWidth()) {
-
-            Text(text = "Click to View",
-                fontSize = 25.sp)
-        }
         }
     }
-
-
-
-
-
-
-
+}
 @Preview
 @Composable
-private fun loginprev() {
-    login()
+private fun registerprev() {
+    register()
 }
